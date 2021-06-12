9d0130b39170043ad7d06a33298589c0a1f66812
Ashan K Nimsara <ashanknimsara@gmail.com>
2021-06-12 12:21:48 +0530

    Add files via upload

---
+++ Salery Report/createReport.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<htmll>
+<head>
+	<title> salery sheet </title>
+</head>
+	<body>
+	
+		<h3>Salary Sheet for November</h3><br>
+		<table>
+		<tr>
+			<th>Employee ID</th>
+			<th>Employee Name</th>
+			<th>Basic Salary</th>
+			<th>OT Rate</th>
+			<th>OT Hours</th>
+			<th>Salary with OT</th>
+		</tr>
+		<?php 
+	
+		$conn = mysqli_connect("localhost","root","user321","employee");
+		$records = mysqli_query($conn,"select * from salary tabale");
+		
+		while($data = mysqli_fetch_array($records))
+		{
+		 $OTRate = $data['OTRate'];
+         $OTHours = $data['OTHours'];
+		 $BasicSal = $data['BasicSalary'];
+		 
+         $TotalSal = ($OTrate*$OTHours)+$BasicSal;
+		 
+		 $month = $data['Month'];
+		 if($month==11){
+		 
+		?>
+		<tr>
+			<td><?php echo $data['empID']; ?></td>
+			<td><?php echo $data['empName']; ?></td>
+			<td><?php echo $BasicSal ?></td>
+			<td><?php echo $OTRate ?></td>
+			<td><?php echo $OTHours ?></td>
+			<td><?php echo $TotalSal ?></td>
+		</tr>
+		<?php
+			}
+		}
+		?>
+		
+		</table>
+
+	</body>
+
+</html>
